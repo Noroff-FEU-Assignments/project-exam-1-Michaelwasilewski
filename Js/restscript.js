d0a869d5b72e45034aefe02db9db09746d238052
Michael Wasilewski <michal.wasilewski@stud.noroff.no>
2022-05-19 21:59:54 +0200

    Update restscript.js

--- Js/restscript.js
+++ Js/restscript.js
@@ -9,6 +9,12 @@ async function getBlogs(){
         console.log(blogData);
         blogContainer.innerHTML = "";
         for (let i = 0; i < blogData.length; i++) {
+            let blogPicture = blogData[i].images[0].src;
+            let altText = blogData[i].images[0].alt;
+            let blogPrice = blogData[i].prices.price;
+            let currency = blogData[i].prices.currency_code;
+            let blogName = blogData[i].name;
+            gamesContainer.innerHTML += 
         }
     } catch (error) {
         console.log ("ops, there is an error", error);
