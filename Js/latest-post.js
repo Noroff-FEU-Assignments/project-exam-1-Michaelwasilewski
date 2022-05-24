b06c17ef8a6e41d7d41c98cb0f7127166fc46dd9
Michael Wasilewski <michal.wasilewski@stud.noroff.no>
2022-05-24 21:44:43 +0200

    fixed carousel, small changes to css

--- Js/latest-post.js
+++ Js/latest-post.js
@@ -1,22 +1,80 @@
-const API = "https://gifted-signature.flywheelsites.com/wp-json/wp/v2/posts?per_page=1"
+const API =
+  "https://gifted-signature.flywheelsites.com/wp-json/wp/v2/posts?per_page=1";
 
 const latestContainer = document.querySelector(".split-section");
+
+const carouselContainer = document.getElementById("content");
+
+const APIcarousel =
+  "https://gifted-signature.flywheelsites.com/wp-json/wp/v2/posts?per_page=10";
+
+async function getBlogs() {
+  try {
+    const response = await fetch(APIcarousel);
+    const blogData = await response.json();
+    carouselContainer.innerHTML = "";
+    for (let i = 0; i < blogData.length; i++) {
+      let blogPicture = blogData[i].x_featured_media_original;
+      let altText = blogData[i].x_metadata.alt_text;
+      carouselContainer.innerHTML += `
+        
+            <div id="content">
+                <a href="blogspecific.html?id=${blogData[i].id}"><img class="item" src="${blogPicture}" alt="${altText}"></a>
+            </div>
+              `;
+    }
+  } catch (error) {
+    console.log("ops, there is an error", error);
+  }
+}
+getBlogs();
+
+
+
+const gap = 16;
+
+const carousel = document.getElementById("carousel"),
+  content = document.getElementById("content"),
+  next = document.getElementById("next"),
+  prev = document.getElementById("prev");
+
+next.addEventListener("click", (e) => {
+  carousel.scrollBy(width + gap, 0);
+  if (carousel.scrollWidth !== 0) {
+    prev.style.display = "flex";
+  }
+  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "none";
+  }
+});
+prev.addEventListener("click", (e) => {
+  carousel.scrollBy(-(width + gap), 0);
+  if (carousel.scrollLeft - width - gap <= 0) {
+    prev.style.display = "none";
+  }
+  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "flex";
+  }
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", (e) => (width = carousel.offsetWidth));
 
 
 
 async function getLatestBlogs() {
-    try {
-      const response = await fetch(API);
-      const blogData = await response.json();
-      latestContainer.innerHTML = "";
-      for (let i = 0; i < blogData.length; i++) {
-        let blogPicture = blogData[i].x_featured_media_original;
-        console.log(blogPicture);
-        let altText = blogData[i].x_metadata.alt_text;
-        let blogName = blogData[i].acf.title;
-        let blogDescription = blogData[i].acf.paragraf;
-        let blogDate = blogData[i].x_date;
-        latestContainer.innerHTML += `
+  try {
+    const response = await fetch(API);
+    const blogData = await response.json();
+    latestContainer.innerHTML = "";
+    for (let i = 0; i < blogData.length; i++) {
+      let blogPicture = blogData[i].x_featured_media_original;
+      console.log(blogPicture);
+      let altText = blogData[i].x_metadata.alt_text;
+      let blogName = blogData[i].acf.title;
+      let blogDescription = blogData[i].acf.paragraf;
+      let blogDate = blogData[i].x_date;
+      latestContainer.innerHTML += `
               <div class="split-section">
                   <div class="container-split">
                       <div class="featured-picture">
@@ -34,10 +92,12 @@ async function getLatestBlogs() {
                   </div>
               </div>
               `;
-      }
-    } catch (error) {
-      console.log("ops, there is an error", error);
     }
+  } catch (error) {
+    console.log("ops, there is an error", error);
   }
-  
-  getLatestBlogs();
+}
+
+getLatestBlogs();
+
+
