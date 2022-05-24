131c14fe43a7302e34374ad504a79b58e033e935
Michael Wasilewski <michal.wasilewski@stud.noroff.no>
2022-05-24 12:41:27 +0200

    modal buggin

--- Js/detail.js
+++ Js/detail.js
@@ -4,28 +4,37 @@ const params = new URLSearchParams(queryString);
 
 const id = params.get("id");
 
-const APIUrl = `https://gifted-signature.flywheelsites.com/wp-json/wc/store/products/${id}`;
+const APIUrl = `https://gifted-signature.flywheelsites.com/wp-json/wp/v2/posts/${id}`;
 
 const blogDetails = document.querySelector(".split-section");
 
-async function getBlogDetails () {
-    try {
-        const response = await fetch (APIUrl);
-        const singleBlogDetail = await response.json();
-        let blogPicture = singleBlogDetail.images[0].src;
-        let altText = singleBlogDetail.images[0].alt;
-        let blogTitle = singleBlogDetail.name;
-        let bloginfo = singleBlogDetail.short_description;
-        blogDetails.innerHTML += `
-            <div class="split-section">
+
+
+
+
+async function getBlogDetails() {
+  try {
+    const response = await fetch(APIUrl);
+    const singleBlogDetail = await response.json();
+    let blogPicture = singleBlogDetail.x_featured_media_original;
+    let altText = singleBlogDetail.x_metadata.alt_text;
+    let blogTitle = singleBlogDetail.x_metadata.title;
+    let bloginfo = singleBlogDetail.x_metadata.paragraf;
+    let blogDate = singleBlogDetail.x_date;
+    blogDetails.innerHTML += `
                 <div class="container-split">
-                    <div class="featured-picture">
-                        <img src="${blogPicture}" alt="${altText}">
+                    <div class="featured-picture" >
+                    <img id="myImg" src="${blogPicture}" alt="${altText}" >
+                    </div>
+                    <div id="myModal" class="modal">
+                    <span class="closed">&times;</span>
+                    <img  src="${blogPicture}" class="modalContent" id="img01" />
+                    <div id="caption">${altText}</div>
                     </div>
                     <div class="featured-info">
                         <div class="post-date">
                             <span>Tuesday</span>
-                            <span>19.05.2022</span>
+                            <span>${blogDate}</span>
                         </div>
                         <h2 class="featured-title">${blogTitle}</h2>
                         <div class="product-rating">
@@ -37,16 +46,32 @@ async function getBlogDetails () {
                             <span>4.5(65)</span>
                         </div>
                         <p class="featured-text">${bloginfo}</p>
-                        <h2 class="featured-title"> Pro's & Cons </h2>
+                        <h2 class="featured-title">Pro's & Cons</h2>
                         <p class="featured-text">${bloginfo}</p>
-                        <a class="featured-cta" href="/blogspecific.html">Visit site</a>
+                        <a class="featured-cta" href="#">Visit site</a>
                     </div>
                 </div>
-            </div>
-        `
-    } catch (error) {
-        console.log(error);
-    }
-    
+        `;
+  } catch (error) {
+    console.log(error);
+  } finally {
+      const modal = document.querySelector(".modal");
+      const image = document.getElementById("myImg");
+
+      image.onclick = function () {
+          modal.style.display = "block";
+      }
+  }
 }
 getBlogDetails();
+getBlogDetails().then(() => {
+    const closeModal = document.querySelector(".closed");
+console.log(closeModal);
+    closeModal.onclick = function () {
+        console.log("hello");
+    //   modal.style.display = "none";
+    }
+}) ;
+
+
+
--- Js/restscript.js
+++ Js/restscript.js
@@ -1,5 +1,5 @@
 const APIUrl =
-  "https://gifted-signature.flywheelsites.com/wp-json/wc/store/products";
+  "https://gifted-signature.flywheelsites.com/wp-json/wp/v2/posts";
 
 const blogContainer = document.querySelector(".blog-container");
 
@@ -7,24 +7,25 @@ async function getBlogs() {
   try {
     const response = await fetch(APIUrl);
     const blogData = await response.json();
-    console.log(blogData);
     blogContainer.innerHTML = "";
     for (let i = 0; i < blogData.length; i++) {
-      let blogPicture = blogData[i].images[0].src;
-      let altText = blogData[i].images[0].alt;
-      let blogName = blogData[i].name;
-      let blogDescription = blogData[i].short_description;
+      let blogPicture =  blogData[i].x_featured_media_original;
+      console.log(blogPicture);
+      let altText = blogData[i].x_metadata.alt_text;
+      let blogName = blogData[i].acf.title;
+      let blogDescription = blogData[i].acf.paragraf;
+      let blogDate = blogData[i].x_date
       blogContainer.innerHTML += 
       `
             <div class="split-section">
                 <div class="container-split">
                     <div class="featured-picture">
                     <img src="${blogPicture}" alt="${altText}">
-                        </div>
+                    </div>
                     <div class="featured-info">
                         <div class="post-date">
                             <span>Tuesday</span>
-                                 <span>19.05.2022</span>
+                            <span>${blogDate}</span>
                          </div>
                             <h2 class="featured-title">${blogName}</h2>
                             <p class="featured-text"> ${blogDescription}</p>
