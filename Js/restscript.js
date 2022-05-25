30f960ea1e77e2c55e0a2bc85375235d81d5286f
Michael Wasilewski <michal.wasilewski@stud.noroff.no>
2022-05-25 11:07:01 +0200

    fixed*2

--- Js/restscript.js
+++ Js/restscript.js
@@ -5,7 +5,7 @@ const blogContainer = document.querySelector(".blog-container");
 
 const loadMore = document.getElementById("load-more");
 const ApiMore =
-  "https://gifted-signature.flywheelsites.com/wp-json/wp/v2/posts?per_page=12&offset=10";
+  "https://gifted-signature.flywheelsites.com/wp-json/wp/v2/posts?_page=2&offset=10";
 
 async function getBlogs() {
   try {
@@ -50,7 +50,7 @@ async function getMoreBlogs() {
   try {
     const response = await fetch(ApiMore);
     const blogData = await response.json();
-    // blogContainer.innerHTML = "";
+
     for (let i = 0; i < blogData.length; i++) {
       let blogPicture = blogData[i].x_featured_media_original;
       console.log(blogPicture);
